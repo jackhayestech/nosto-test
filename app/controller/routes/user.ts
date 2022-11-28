38928b49503dcc18596f31430cd8186d8536adf5
Jack <contact@jackhayes.tech>
2022-11-28 15:48:06 +1100

    Blanked out user password.

--- app/controller/routes/user.ts
+++ app/controller/routes/user.ts
@@ -1,15 +1,22 @@
 import {TestData} from "../../../no_touching/test_data";
 import { Request, Response } from 'express';
 
+ /**
+ * Retrieve data about the logged in user
+ * @param req
+ * @param res
+ */
 export default function userRoute(req: Request, res: Response) {
+
     res.send(retrieveLoggedInUserData());
 }
 
 function retrieveLoggedInUserData() {
-    /**
-     * Retrieve data about the logged in user
-     * @param req
-     * @param res
-     */
-    return JSON.stringify(TestData.initUser().getUser());
+    const userData = TestData.initUser().getUser();
+
+    // Blanked out customer password as this should be confidential information.
+    // NOTE: Again this might be a business decision depending on the nature of this route but I would highly recommend this regardless.
+    userData.password = 'Confidential information';
+
+    return JSON.stringify(userData);
 }
