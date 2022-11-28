5e6dcb8493cf57151da66df96de745f77a889cfb
Jack <contact@jackhayes.tech>
2022-11-28 15:43:22 +1100

    Fixed issue I introduced during error testing.

--- app/controller/social/facebook.ts
+++ app/controller/social/facebook.ts
@@ -5,7 +5,7 @@ export default class FacebookProvider extends Network {
         super();
 
         this.setAPIEndpoint('https://graph.facebook.com/v2.8/');
-        this.setAccessToken('edG9tYXRvbGV0dHVjZTEyM3h4eXpmYWNlYm9vaw==');
+        this.setAccessToken('dG9tYXRvbGV0dHVjZTEyM3h4eXpmYWNlYm9vaw==');
         this.setAPIName('Facebook');
     }
 }
